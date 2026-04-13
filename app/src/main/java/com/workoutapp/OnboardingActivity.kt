7324e4fd3e7aa1d65e103ac4dee64a63fc24c63d
abdur-rehman <abdurrehman4415@gmail.com>
2026-04-14 01:30:49 +0500

    1) Initialized Project. 2) Onboarding Screen Complete

---
+++ app/src/main/java/com/workoutapp/OnboardingActivity.kt
@@ -0,0 +1,40 @@
+package com.workoutapp
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.lifecycleScope
+import com.ncorti.slidetoact.SlideToActView
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class OnboardingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_onboarding_screen)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val welcomeSlider = findViewById<SlideToActView>(R.id.welcome_slider);
+
+        welcomeSlider.onSlideCompleteListener = object : SlideToActView.OnSlideCompleteListener {
+            override fun onSlideComplete(view: SlideToActView) {
+                lifecycleScope.launch {
+                    delay(700)
+                    val intent = Intent(this@OnboardingActivity, MainActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                }
+            }
+        }
+
+    }
+}
